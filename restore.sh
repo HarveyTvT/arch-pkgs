9b5428495e6b15ea97e7561bb8454b536c2dfe0b
Harvey Shih <harveytvt@gmail.com>
2024-03-26 19:47:54 +0800

    update restore

--- restore.sh
+++ restore.sh
@@ -1,4 +1,3 @@
 #sudo pacman -S --needed - < pkglist-repo.txt
-for x in $(< pkglist-aur.txt); do echo y | yay --answerdiff None --answerclean All --mflags --needed --mflags "--noconfirm"  -Syu $x; done
-for x in $(< pkglist-repo.txt); do echo y | yay --answerdiff None --answerclean All --mflags --needed --mflags "--noconfirm"  -Syu $x; done
-xargs flatpak install -y < pkglist-flatpak.txt
+yay -S --needed --noconfirm - < pkglist-aur.txt
+#xargs flatpak install -y < pkglist-flatpak.txt
