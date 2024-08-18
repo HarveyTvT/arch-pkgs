ad0b7d2cca7c08efe16eb225c1d0e795987e2d45
Harvey Shih <harveytvt@gmail.com>
2024-08-19 06:44:52 +0800

    update golang

--- restore.sh
+++ restore.sh
@@ -1,31 +1,16 @@
 #! /bin/bash
 
 # official package
-sudo pacman -S --needed - < pkglist-repo.txt
+sudo pacman -S --needed - <pkglist-repo.txt
 
 # aur package
 sudo rm -rf yay
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-cd .. 
-yay -S --needed --noconfirm - < pkglist-aur.txt
+cd ..
+yay -S --needed --noconfirm - <pkglist-aur.txt
 
 # flatpak
-xargs flatpak install -y < pkglist-flatpak.txt
-
-# zsh
-sudo pacman -S --needed zsh
-rm -rf ~/.oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git config --global --add oh-my-zsh.hide-dirty 1
-git config --global oh-my-zsh.hide-info 1
-
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-
-sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/g" ~/.zshrc
+xargs flatpak install -y <pkglist-flatpak.txt
 
 # miniconda
 mkdir -p ~/.miniconda3
@@ -47,9 +32,38 @@ if [ ! -f "/usr/bin/vi" ]; then cd /usr/bin && sudo ln -s vim vi; fi
 sudo curl -fL https://app.getambassador.io/download/tel2oss/releases/download/v2.17.0/telepresence-linux-amd64 -o /usr/local/bin/telepresence
 sudo chmod a+x /usr/local/bin/telepresence
 
+# golang config
+cd ~/Downloads
+wget https://go.dev/dl/go1.20.14.linux-amd64.tar.gz
+tar -zvxf go1.20.14.linux-amd64.tar.gz
+sudo mv go /opt/go@1.20.14
+go env -w GOROOT=/opt/go@1.20.14
+go env -w GOPATH=$HOME/go
+go env -w GOPROXY=https://goproxy.cn,direct
+go env -w CGO_ENABLED="0"
+go env -w GOSUMDB=off
+go env -w GO111MODULE=on
+git config --global --add url."git@g.echo.tech:".insteadOf "http://g.echo.tech/"
+git config --global --add url."git@g.echo.tech:".insteadOf "https://g.echo.tech/"
+
 # onedriver
 export MOUNTPOINT=~/OneDrive
 export SERVICE_NAME=$(systemd-escape --template onedriver@.service --path $MOUNTPOINT)
 cp /usr/lib/systemd/user/onedriver@.service $HOME/.config/systemd/user/$SERVICE_NAME
 systemctl --user enable $SERVICE_NAME
 systemctl --user start $SERVICE_NAME
+
+# zsh
+sudo pacman -S --needed zsh
+rm -rf ~/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git config --global --add oh-my-zsh.hide-dirty 1
+git config --global oh-my-zsh.hide-info 1
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/g" ~/.zshrc
